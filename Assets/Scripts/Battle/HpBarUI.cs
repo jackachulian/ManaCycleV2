dcf4b6d38fd55f50a57c90afa9ba6cfac01d0ab2
Jack Caesar <jackachulian@gmail.com>
2025-01-21 21:29:15 -0600

    HP number

--- Assets/Scripts/Battle/HpBarUI.cs
+++ Assets/Scripts/Battle/HpBarUI.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using TMPro;
 
 /// <summary>
 /// Manages the UI of the HP Bar.
 /// Does NOT manage receiving damage, that is managed by the Board. This is a UI only behaviour.
 /// </summary>
 public class HpBarUI : MonoBehaviour {
+    [SerializeField] private TMP_Text healthNumberLabel;
+
     private Material material;
 
     private void Awake() {
@@ -12,6 +15,9 @@ public class HpBarUI : MonoBehaviour {
     }
 
     public void UpdateUI(float hp, float maxHp, int[] incomingDamage) {
+        if (healthNumberLabel) healthNumberLabel.text = hp + "";
+
+
         float hpPercentage = hp / maxHp;
 
         material.SetFloat("_HpPercentage", hpPercentage);
