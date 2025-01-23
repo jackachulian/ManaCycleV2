9a9e1b28a84a243c89c5a115ef13fc0f63933797
mjjm4d <threehalves1@gmail.com>
2025-01-23 14:20:51 -0600

    Add button animations

--- Assets/Scripts/Battle/PauseMenuUI.cs
+++ Assets/Scripts/Battle/PauseMenuUI.cs
@@ -7,11 +7,22 @@ using TMPro;
 public class PauseMenuUI : MonoBehaviour {
     public BattleManager battleManager;
     [SerializeField] private GameObject firstSelectedObject;
+    [SerializeField] private GameObject buttonsParent;
 
     public bool menuShown {get; private set;} = false;
 
     void Awake() {
         if (!menuShown) gameObject.SetActive(false);
+
+        // set every other button animation to be fliped
+        for (int i = 0; i < buttonsParent.transform.childCount; i++)
+        {
+            // flip button
+            Transform t = buttonsParent.transform.GetChild(i);
+            t.localScale -= new Vector3(i % 2 * 2, 0, 0);
+            // flip text again so it is readable
+            t.GetChild(1).localScale -= new Vector3(i % 2 * 2, 0, 0);
+        }
     }
 
 
